00041c9fa636925f8e96e2acc0ab647b42a3ba16
sharpevo <sharpevo@outlook.com>
2016-07-04 14:06:57 +0800

    Add logout controller and views

---
+++ controllers/logout.go
@@ -0,0 +1,41 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+	"ssologin/models"
+)
+
+type LogoutController struct {
+	BaseController
+}
+
+func (c *LogoutController) Get() {
+
+	userInfo := c.GetUserInfo()
+
+	user := models.User{}
+	if code, err := user.FindById(userInfo.Id); err != nil {
+		beego.Error("FindUserById:", err)
+		if code == models.ERROR_NOT_FOUND {
+			beego.Error("No such user.")
+		} else {
+			beego.Error("Database Error.")
+		}
+		return
+	}
+
+	userAgent := c.ParseUserAgent()
+
+	beego.Debug("UserTokens:", user.Tokens)
+	ok := user.RemoveToken(userAgent)
+	if !ok {
+		beego.Error("Logout Fail")
+		return
+	}
+	beego.Debug("UserLogout:", user.Email)
+	beego.Debug("UserTokens:", user.Tokens)
+	returnTo := "www.igenetech.com"
+	c.Data["ReturnTo"] = returnTo
+	c.TplName = "logout.tpl"
+	return
+}
---
+++ views/logout.tpl
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <script>
+            function redirect(){
+                window.location.href = "http://{{.ReturnTo}}";         
+            }
+        </script>
+    </head>
+    <body onload="redirect()">
+        <p>Please wait...</p>
+    </body>
+</html>
