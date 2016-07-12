628acbbe077d3640b1d67067c715c4ed8ce9d573
sharpevo <sharpevo@outlook.com>
2016-07-12 17:08:16 +0800

    Add FormatDate func

--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	"time"
 	"uic/mongo"
 	_ "uic/routers"
 )
+
+func FormatDate(date time.Time) (result string) {
+	if date.IsZero() {
+		return "-"
+	}
+	layout := "15:04 on Jan 02, 2006"
+	return date.Format(layout)
+}
 
 func main() {
 	err := mongo.Startup()
 	if err != nil {
 		beego.Error(err)
 	}
+
+	beego.AddFuncMap("Format", FormatDate)
 
 	beego.Run()
 
