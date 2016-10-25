0719483d90e73f69850c4a7eda72d21f8fbff60b
sharpevo <sharpevo@outlook.com>
2016-10-25 14:59:08 +0800

    Add default value to options

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ sed 's,mongodb.*,'mongodb="${MONGO_DB:-uic}"',g' -i conf/app.conf
 sed 's,mongohost.*,'mongohost="${MONGO_HOST:-db:27017}"',g' -i conf/app.conf
 sed 's,mongouser.*,'mongouser="${MONGO_USER:-uic}"',g' -i conf/app.conf
 sed 's,mongopassword.*,'mongopassword="${MONGO_PASSWORD:-Goo7ieho}"',g' -i conf/app.conf
-sed 's,uicdomain.*,'uicdomain="${UIC_DOMAIN}"',g' -i conf/app.conf
-sed 's,appdomains.*,'appdomains="${APP_DOMAINS}"',g' -i conf/app.conf
-sed 's,signupenabled.*,'signupenabled="${SIGNUP}"',g' -i conf/app.conf
+sed 's,uicdomain.*,'uicdomain="${UIC_DOMAIN:-accounts.igenetech.com}"',g' -i conf/app.conf
+sed 's,appdomains.*,'appdomains="$APP_DOMAINS"',g' -i conf/app.conf
+sed 's,signupenabled.*,'signupenabled="${SIGNUP:-false}"',g' -i conf/app.conf
 uic
