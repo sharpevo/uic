7e77707561ba243b29a1692391be62af5886c9ec
sharpevo <sharpevo@outlook.com>
2016-11-02 13:51:20 +0800

    Add UserAppController router

--- routers/router.go
+++ routers/router.go
@@ -16,6 +16,7 @@ func init() {
 	beego.Router("/logout", &controllers.LogoutController{})
 	beego.Router("/roles", &controllers.RoleController{})
 	beego.Router("/apps", &controllers.AppController{})
+	beego.Router("/userapp", &controllers.UserAppController{})
 	beego.Router("/profile", &controllers.ProfileController{})
 	ns := beego.NewNamespace(
 		"v1",
