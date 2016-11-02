e1141d8c37a615effd8287c090ec45c85e83a32f
sharpevo <sharpevo@outlook.com>
2016-11-02 08:58:04 +0800

    Change Post method of AppController

--- controllers/apps.go
+++ controllers/apps.go
@@ -10,62 +10,63 @@ type AppController struct {
 	BaseController
 }
 
+func (c *AppController) Get() {
+	beego.ReadFromRequest(&c.Controller)
+	c.Data["Apps"] = models.GetAllApps()
+	c.Layout = "layout.tpl"
+	c.TplName = "app.tpl"
+}
+
 func (c *AppController) Post() {
 	flash := beego.NewFlash()
-	userId := c.GetString("userId")
-	appName := strings.Replace(
-		strings.ToLower(c.GetString("appName")),
-		" ",
-		"",
-		-1)
-	appName = strings.Replace(appName, ".", " ", -1)
+	c.Layout = "layout.tpl"
+	c.TplName = "app.tpl"
 
-	user := models.User{}
-	if code, err := user.FindById(userId); err != nil {
-		beego.Error("FindUserById:", err)
-		if code == models.ERROR_NOT_FOUND {
-			flash.Error("No such user.")
-		} else {
-			flash.Error("Database Error.")
-		}
+	appName := c.GetString("appName")
+	appDomain := c.GetString("appDomain")
+	appRemark := c.GetString("appRemark")
+
+	if appName == "" || appDomain == "" {
+		flash.Error("Invaild input.")
 		flash.Store(&c.Controller)
 		c.Redirect(
-			c.URLFor(".Get"),
+			c.URLFor(
+				".Get",
+				"appName", appName,
+				"appDomain", appDomain),
 			302)
 		return
 	}
 
-	if !user.Apps[appName] {
-		beego.Debug("AddApp:", appName)
-		err := user.AddApp(appName)
-		if err != nil {
-			beego.Error("AddApp:", err)
-			flash.Error("Fail to add app.")
-			flash.Store(&c.Controller)
-			c.Redirect(
-				c.URLFor("RoleController.Get"),
-				302)
-			return
-		}
+	app := models.App{
+		Name:    appName,
+		Domain:  appDomain,
+		Enabled: true,
+		Remark:  appRemark,
+	}
 
-	} else {
-		beego.Debug("RemoveApp:", appName)
-		err := user.RemoveApp(appName)
-		if err != nil {
-			beego.Error("RemoveApp:", err)
-			flash.Error("Fail to add app.")
-			flash.Store(&c.Controller)
-			c.Redirect(
-				c.URLFor("RoleController.Get"),
-				302)
-			return
+	if code, err := app.Create(); err != nil || code != 0 {
+		if code == models.ERROR_DUPLICATE {
+			beego.Error("Domain has been registered.")
+			flash.Error("Domain has been registered.")
+		} else {
+			beego.Error("Unknown Error.")
+			flash.Error("Fail to create domain.")
 		}
+		flash.Store(&c.Controller)
+		c.Redirect(
+			c.URLFor(
+				".Get",
+				"appName", appName,
+				"appDomain", appDomain),
+			302)
+		return
 	}
 
 	flash.Notice("Success!")
 	flash.Store(&c.Controller)
 	c.Redirect(
-		c.URLFor(
-			"RoleController.Get"),
+		c.URLFor(".Get"),
 		302)
+	return
 }
