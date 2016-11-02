9229c5c65d00a6fb7c7569c85587b3628dd67fb0
sharpevo <sharpevo@outlook.com>
2016-11-02 14:28:06 +0800

    Add edit process in AppController

--- controllers/apps.go
+++ controllers/apps.go
@@ -25,27 +25,60 @@ func (c *AppController) Post() {
 	c.Layout = "layout.tpl"
 	c.TplName = "app.tpl"
 
-	appId := c.GetString("appId")
-	if appId != "" {
-		app := models.App{}
-		app.FindById(appId)
-		err := app.Delete()
-		beego.Debug("Remove App", app.Domain)
-		if err != nil {
-			beego.Error(err)
-			flash.Error("Failed to delete app.")
-		} else {
-			flash.Notice("Success!")
-		}
-		flash.Store(&c.Controller)
-		c.Redirect(
-			c.URLFor(".Get"), 302)
-		return
-	}
-
 	appName := c.GetString("appName")
 	appDomain := c.GetString("appDomain")
 	appRemark := c.GetString("appRemark")
+	appEnabled := c.GetString("appEnabled")
+
+	appId := c.GetString("appId")
+	if appId != "" {
+		app := models.App{}
+		err := app.FindById(appId)
+		if err != nil {
+			beego.Error("FindAppById:", err)
+			flash.Error("No such app.")
+			flash.Store(&c.Controller)
+			c.Redirect(c.URLFor(".Get"), 302)
+			return
+		}
+		// edit
+		if appName != "" {
+			app.Name = appName
+			app.Domain = appDomain
+			app.Remark = appRemark
+			if appEnabled == "on" {
+				app.Enabled = true
+			} else {
+				app.Enabled = false
+			}
+			_, err = app.Update()
+			if err != nil {
+				beego.Error("UpdateApp:", err)
+				flash.Error("Failed to update app.")
+				flash.Store(&c.Controller)
+				c.Redirect(c.URLFor(".Get"), 302)
+				return
+			}
+			flash.Notice("Success!")
+			flash.Store(&c.Controller)
+			c.Redirect(c.URLFor(".Get"), 302)
+			return
+		} else {
+			// delete
+			err := app.Delete()
+			beego.Debug("Remove App", app.Domain)
+			if err != nil {
+				beego.Error(err)
+				flash.Error("Failed to delete app.")
+			} else {
+				flash.Notice("Success!")
+			}
+			flash.Store(&c.Controller)
+			c.Redirect(
+				c.URLFor(".Get"), 302)
+			return
+		}
+	}
 
 	if appName == "" || appDomain == "" {
 		flash.Error("Invaild input.")
